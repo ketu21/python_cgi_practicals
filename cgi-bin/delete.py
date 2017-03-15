cabee6be558e7795de60ae6b4aa5904d096d9e0a
ketu21 <ketul@Ketuls-Air.attlocal.net>
2017-03-15 15:23:58 -0700

    Revert "Data Insertion"
    This reverts commit 33c4879e13dc2a32ce42fdb39d35f0b0da7c9a8f.

--- cgi-bin/delete.py
+++ cgi-bin/delete.py
@@ -4,45 +4,28 @@
 import cgi, cgitb
 import mysql.connector
 
-def DataBase():
-    '''Function to handle database connection and other operations'''
-    return mysql.connector.connect(user="root", password="pramukhraj", host="localhost", database="sys")
-
-
 cgitb.enable()
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
 
 # Get data from fields
-student_id = int(form.getvalue('student_id'))
+sid = int(form.getvalue('Sid'))
+sname = form.getvalue('Sname')
 
-if student_id !=0:
-    # Query to delete from database
-    query = "DELETE FROM Student WHERE studentId =('%d')" % (student_id)
+db = mysql.connector.connect(user="root", password="pramukhraj", host="localhost", database="sys")
+cursor= db.cursor()
 
-    db = DataBase()
-    cursor = db.cursor()
-    cursor.execute(query)
-    db.commit()
-    db.close()
+query = "DELETE FROM Student WHERE Sid =('%d')" % (sid)
+cursor.execute(query)
+db.commit()
+db.close()
 
-    print("Content-type:text/html\r\n\r\n")
-    print("<html>")
-    print("<head>")
-    print("<title>Delete Operation</title>")
-    print("</head>")
-    print("<body>")
-    print("<h2>Record Deleted</h2>")
-    print("</body>")
-    print("</html>")
-
-else:
-    print("Content-type:text/html\r\n\r\n")
-    print("<html>")
-    print("<head>")
-    print("<title>Insert Operation</title>")
-    print("</head>")
-    print("<body>")
-    print("<h2>No Student Id Entered</h2>")
-    print("</body>")
-    print("</html>")
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %d %s Deleted</h2>" % (sid, sname))
+print ("</body>")
+print ("</html>")
--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -4,51 +4,28 @@
 import cgi, cgitb
 import mysql.connector
 
-
-def DataBase():
-    '''Function to handle database connection '''
-    return mysql.connector.connect(user="root", password="pramukhraj", host="localhost", database="sys")
-
-
 cgitb.enable()
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
 
-# Get data from fields from the html form
-studentId = int(form.getvalue('student_id'))
-studentName = form.getvalue('student_name')
-studentCity = form.getvalue('student_city')
-studentDate = str(form.getvalue('student_date'))
+# Get data from fields
+sid = int(form.getvalue('Sid'))
+sname = form.getvalue('Sname')
 
-if studentId != 0:
-    # Insert query
-    query = "INSERT INTO Student" \
-            "(studentId,studentName,studentCity,studentBirthDate) " \
-            "VALUES ('%d','%s','%s','%s' )" \
-            % (studentId, studentName, studentCity, studentDate)
+db = mysql.connector.connect(user="root", password="pramukhraj", host="localhost", database="sys")
+cursor= db.cursor()
 
-    db = DataBase()
-    cursor = db.cursor()
-    cursor.execute(query)
-    db.commit()
-    db.close()
+query = "INSERT INTO Student(sid,sname) VALUES ('%d','%s' )" % (sid, sname)
+cursor.execute(query)
+db.commit()
+db.close()
 
-    print("Content-type:text/html\r\n\r\n")
-    print("<html>")
-    print("<head>")
-    print("<title>Insert Operation</title>")
-    print("</head>")
-    print("<body>")
-    print("<h2>Record Inserted</h2>")
-    print("</body>")
-    print("</html>")
-else:
-    print("Content-type:text/html\r\n\r\n")
-    print("<html>")
-    print("<head>")
-    print("<title>Insert Operation</title>")
-    print("</head>")
-    print("<body>")
-    print("<h2>Insert Error Try again</h2>")
-    print("</body>")
-    print("</html>")
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello Id: %d Name: %s Inserted</h2>" % (sid, sname))
+print ("</body>")
+print ("</html>")
